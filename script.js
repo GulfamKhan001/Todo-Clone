58417797a97ac2d8d01222924f7572423485616c
GulfamKhan001 <gulfamkajla@gmail.com>
2022-05-13 09:48:35 +0530

    :zap: ticket Container Created

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const addBtn=document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
 
+let colors = ['lightpink', 'lightgreen', 'lightblue', 'black'];
+let modalPriorityColor = colors[colors.length - 1]; //black
+let textAreaCont = document.querySelector(".textarea-cont");
+const mainCont = document.querySelector(".main-cont");
+
 let addModal=false;
 addBtn.addEventListener('click',function () {
     if (!addModal) {
@@ -19,6 +24,34 @@ allPriorityColors.forEach(function(colorElement){
         allPriorityColors.forEach(function (priorityColorElem){
             priorityColorElem.classList.remove("active");
         })
-        colorElement.classList.add("active")
+        colorElement.classList.add("active");
+        modalPriorityColor = colorElement.classList[0];
     })
 })
+
+modalCont.addEventListener("keydown", function (e) {
+    let key = e.key;
+    if (key == "Shift") {
+      console.log(modalPriorityColor);
+      console.log(textAreaCont.value);
+      createTicket(modalPriorityColor, textAreaCont.value);
+      modalCont.style.display = "none";
+        isModalPresent = false;
+        textAreaCont.value = "";
+        allPriorityColors.forEach(function (colorElem) {
+            colorElem.classList.remove("active");
+        })
+    }
+});
+
+function createTicket(ticketColor, data) {
+    let ticketCont = document.createElement("div"); //<div></div>
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `
+        <div class="ticket-color ${ticketColor} "></div>
+        <div class="ticket-id"></div>
+        <div class="task-area">${data}</div>
+    `;
+
+    mainCont.appendChild(ticketCont);
+}
